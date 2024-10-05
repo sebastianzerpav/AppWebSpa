a957c535f74b7f89ea38403397d30100da4374dd
Sebastián Zerpa Vanegas <sebastianzerpav@gmail.com>
2024-10-04 20:10:09 -0500

    CRUD User Generado automaticamente al crear controlador. Inicio de creación manual de CRUD SpaServices. Front generado por IA.

---
+++ AppWebSpa/Controllers/SpaServicesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AppWebSpa.Data;
+using AppWebSpa.Models;
+namespace AppWebSpa.Controllers
+{
+    public class SpaServicesController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SpaServicesController(AppDbContext context) {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //IEnumerable<SpaService> services = await _context.SpaService;
+            return View();
+        }
+    }
+}
--- AppWebSpa/Program.cs
+++ AppWebSpa/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 //AppDbContext Dependency Injection
-builder.Services.AddDbContext<AppDbContext>(options => 
+builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStringDb")));
 
 builder.Services.AddControllersWithViews();
